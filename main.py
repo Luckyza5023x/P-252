73c69cba5d4a3ddcd30d7a1a50fd293551b935aa
Luckyza5023x <113436103+Luckyza5023x@users.noreply.github.com>
2024-01-27 18:51:18 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,88 @@
+# importing flask modules
+from flask import Flask , request , render_template , jsonify
+
+# importing firebase_admin module
+import firebase_admin
+
+# importing firestore.py module to create firestore client
+from firebase_admin import firestore
+
+# importing credentials.py module from firebase_admin folder
+from firebase_admin import credentials
+
+# creating authentication file
+if not firebase_admin._apps:
+    cred = credentials.Certificate(creds)
+    default_app = initialize_app(cred)
+
+# connect this python script/app with firebase using the authentication credentials
+firebase_admin.initialize_app(cred)
+
+# creating firestore client
+firebase_db = firestore.client()
+
+
+# creating flask object
+app = Flask(__name__)
+
+# first api : index page, only GET requests allowed at this API 
+@app.route("/get-data", methods=["GET"])
+def get_data():
+    try:
+        
+        # getting values from firebase document
+
+        # convert it to python dictionary format
+
+        # extracting value from dictionary
+
+        # rendering index.html template and pass the extracted value
+
+
+
+
+
+# second api : adding data , only POST request allowed at this API
+@app.route("/add-data", methods=["POST"])
+def add_data():
+    try:
+        potentiometer = request.json.get("potentiometer")
+        humidity= request.json.get("humidity")
+        pressure= request.json.get("pressure")
+        altitude= request.json.get("altitude")
+
+        document_ref= firebase_db.collection("data")
+        add_values= document_ref.document().create(dict(potentiometer= potentiometer, date= datetime.datetime.utcnow() ))
+           
+           
+        return jsonify({
+            "status": "success"
+        }), 201
+    except Exception as e:
+        return jsonify({
+            "status": "error",
+            "message": str(e)
+        }), 400
+
+@app.route("/")
+def index():
+   
+   try:
+    document_ref = firebase_db.collection("data")
+    data = document_ref.order_by("date", direction='DESCENDING').limit(1).get()[0].to_dict() 
+    return render_template("/home/home.html", data=data)
+   
+   
+   except Exception as e: 
+        print(str(e)) 
+        return jsonify({ "status": "error", "message": "No data in database yet!" }), 400
+   
+
+
+
+
+
+# start the server
+if __name__  ==  "__main__":
+    app.run(host='192.168.29.67', port=5000)
+
